84e1677616f6162548e974b398713e7c9f5ae53d
Matthies Haentjens <haentjens.matthies@gmail.com>
2020-09-22 21:36:49 +0700

    feature: styled the popup

--- src/client/index.js
+++ src/client/index.js
@@ -1,13 +1,11 @@
 import { openAddTripPopup } from './js/createTravelCard.js';
 import { getTripDetails } from './js/createTravelCard.js';
 import { createCard } from './js/createTravelCard.js';
-// import { performAction } from './js/getLocalWeather.js';
 import { getCoordinates } from './js/getLocalWeather.js';
 import { getWeatherForecast } from './js/getLocalWeather.js';
 import { getHistoricalWeather } from './js/getLocalWeather.js';
 import { getDestinationPicture } from './js/getPicture.js'
 import { postData } from './js/createTravelCard.js';
-// import { updateUI } from './js/getLocalWeather.js';
 
 
 import './styles/styles.scss';
@@ -18,11 +16,9 @@ export {
     openAddTripPopup,
     getTripDetails,
     createCard,
-    // performAction,
     getCoordinates,
     getWeatherForecast,
     getHistoricalWeather,
     getDestinationPicture,
     postData
-    // updateUI
 }
--- src/client/js/getLocalWeather.js
+++ src/client/js/getLocalWeather.js
@@ -15,29 +15,6 @@ const apiKeyWeatherbit = '88034f45d76e48dd9032d139b923cb75';
 // Need to figure out how this works
 // const apiKeyWeatherbit = process.env.API_KEY_WEATHERBIT
 
-// Event listener to add function to existing HTML DOM element
-// document.getElementById('get-weather').addEventListener('click', performAction);
-
-/* Function called by event listener */
-// export async function performAction(e) {
-//     const city = 'Amsterdam'
-//     const country = 'NL'
-//     const data = await Client.getCoordinates(city, country);
-//     console.log(data)
-//     const lat = data.geonames[0].lat
-//     const lng = data.geonames[0].lng
-//     const forecastWeatherData = await Client.getWeatherForecast(lat, lng);
-//     console.log(forecastWeatherData)
-//     const startDate = '2020-01-01'
-//     const endDate = '2020-01-02'
-//     const historicalWeatherData = await Client.getHistoricalWeather(lat, lng, startDate, endDate);
-//     console.log(historicalWeatherData)
-//     const destinationPicture = await Client.getDestinationPicture(city)
-//     console.log(destinationPicture)
-    // await Client.postData('/add', {lat:data.geonames[0].lat});
-    // await Client.updateUI();
-// }
-
 /* Function to GET Geonames API Data*/
 export const getCoordinates = async (city) =>{
     const res = await fetch(baseURLGeonames+city+'&maxRows=1'+'&username='+apiKeyGeoNames);
@@ -72,36 +49,3 @@ export const getHistoricalWeather = async (lat, lng, startDate, endDate) => {
         console.log("error", error);
       }
 }
-
-/* Function to POST data */
-// export const postData = async ( url = '', data = {})=>{
-//     const res = await fetch(url, {
-//     method: 'POST',
-//     credentials: 'same-origin',
-//     headers: {
-//         'Content-Type': 'application/json',
-//     },
-//    // Body data type must match "Content-Type" header
-//     body: JSON.stringify(data),
-//     });
-//     try {
-//       const newData = await res.json();
-//       return newData;
-//     }
-//     catch(error) {
-//       console.log("error", error);
-//     }
-//   }
-
-/* Function to GET Project Data */
-// export const updateUI = async () => {
-//   const req = await fetch('/all');
-//   try{
-//     const allData = await req.json();
-//     document.getElementById('lat').innerHTML = allData[allData.length - 1].lat;
-
-//   }
-//   catch(error){
-//     console.log("error", error);
-//   }
-// }
